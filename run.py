585e8c51cf37e4379313427f9484d9eaf35f6045
harishy0406 <harivelgm76@gmail.com>
2026-01-07 23:04:29 +0530

    Update Main UI and removed navbar

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 from app import create_app, db
 from app.models import User, Driver, Truck, Route, Assignment
-
 app = create_app()
-
 with app.app_context():
     db.create_all()
     # Create admin user if doesn't exist
